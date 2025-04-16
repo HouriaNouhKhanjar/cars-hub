810dc9449dbcc1c135fad3d0fd0e7e26a8351b44
HouriaNouhKhanjar <gls97048@students.codeinstitute.net>
2025-04-16 21:09:08 +0200

    design profile page

--- car/urls.py
+++ car/urls.py
@@ -2,5 +2,9 @@ from . import views as car_views
 from django.urls import path
 
 urlpatterns = [
+    path('profile/', car_views.profile, name='profile'),
+    path('cars-list/', car_views.cars_list, name='cars_list'),
+    path('car-edit/', car_views.car_edit, name='car_edit'),
+    path('liking-list/', car_views.liking_list, name='liking_list'),
     path('', car_views.index, name='home'),
 ]
--- car/views.py
+++ car/views.py
@@ -2,4 +2,31 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    """
+    Load the Homepage
+    """
     return render(request, 'car/index.html')
+
+def profile(request):
+    """
+    Manage profile page
+    """
+    return render(request, 'profile/index.html')
+
+def cars_list(request):
+    """
+    Display users cars list
+    """
+    return render(request, 'profile/cars-list.html')
+
+def car_edit(request):
+    """
+    Manage users car
+    """
+    return render(request, 'profile/cars-edit.html')
+
+def liking_list(request):
+    """
+    display users liking
+    """
+    return render(request, 'profile/liking-list.html')
