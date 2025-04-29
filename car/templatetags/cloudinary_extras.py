5358eebf61dcc50cbd11d3e8a7427c6e4bddd564
HouriaNouhKhanjar <gls97048@students.codeinstitute.net>
2025-04-29 22:25:25 +0200

    improve performance home page

---
+++ car/templatetags/cloudinary_extras.py
@@ -0,0 +1,20 @@
+from django import template
+import re
+
+register = template.Library()
+
+
+@register.filter
+def cloudinary_optimize_car(url):
+    """
+    Inserts f_auto,q_auto right after 'upload/' in the Cloudinary URL.
+    """
+    return re.sub(r'/upload/', '/upload/c_fill,w_400,h_300/f_auto,q_auto/', url.replace("http://", "https://"))
+
+
+@register.filter
+def cloudinary_optimize_category(url):
+    """
+    Inserts f_auto,q_auto right after 'upload/' in the Cloudinary URL.
+    """
+    return re.sub(r'/upload/', '/upload/f_auto,q_auto/', url.replace("http://", "https://"))
