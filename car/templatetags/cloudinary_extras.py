17c356e26aa7e1f57a1c3d81ad10c925fa593a16
HouriaNouhKhanjar <gls97048@students.codeinstitute.net>
2025-04-30 10:10:36 +0200

    edit category filter image url

--- car/templatetags/cloudinary_extras.py
+++ car/templatetags/cloudinary_extras.py
@@ -7,14 +7,18 @@ register = template.Library()
 @register.filter
 def cloudinary_optimize_car(url):
     """
-    Inserts f_auto,q_auto right after 'upload/' in the Cloudinary URL.
+    Replace http with https and
+    inserts f_auto,q_auto and aspect ratio
+    right after 'upload/' in the Cloudinary URL.
     """
-    return re.sub(r'/upload/', '/upload/c_fill,w_400,h_300/f_auto,q_auto/', url.replace("http://", "https://"))
+    return re.sub(r'/upload/', '/upload/c_fill,w_400,h_300/f_auto,q_auto/',
+                  url.replace("http://", "https://"))
 
 
 @register.filter
 def cloudinary_optimize_category(url):
     """
-    Inserts f_auto,q_auto right after 'upload/' in the Cloudinary URL.
+    Replace http with https
+    right after 'upload/' in the Cloudinary URL.
     """
-    return re.sub(r'/upload/', '/upload/c_fill,w_300,h_200/f_auto,q_auto/', url.replace("http://", "https://"))
+    return url.replace("http://", "https://")
